9a6a07e5f34533ac83eac4d8459cd113975ce346
kosakuka <playlikeharden@gmail.com>
2023-03-20 21:35:48 +0900

    apiの不要ファイル削除、APIを連携用のものに変更

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -2,20 +2,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  title: string;
+  text: string;
 };
 
-let hoge = "";
+let title = "";
+let text = "";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req.method === "GET") {
-    res.status(200).json({ name: hoge });
+    res.status(200).json({ title, text });
   } else if (req.method === "POST") {
-    hoge = req.body.name;
-    res.status(200).json({ name: hoge });
+    const result: Data = req.body;
+    title = result.title;
+    text = result.text;
+    res.status(200).json({ title, text });
   } else {
     res.status(404).end();
   }
